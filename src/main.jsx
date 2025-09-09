cf53a6b463760bed38a5839631b08fb8a2ceb2f2
Khalid El-khalil <ekay@Khalids-MacBook-Pro.local>
2025-09-09 23:40:05 +0100

    feat: add CuttimeStarterApp prototype with logo

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CuttimeStarterApp from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CuttimeStarterApp from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CuttimeStarterApp />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
